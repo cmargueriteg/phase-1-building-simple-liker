f97f45309cb061f976b0610fd5916a48f611a029
Cindy Germain <cindymarguerite@gmail.com>
2022-04-09 14:23:03 -0600

    commit tree 74fb911c547d0ea8dcec0d5e1ce24cd748ba3494

--- main.js
+++ main.js
@@ -1,8 +1,54 @@
-// Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const errorModal = document.querySelector("#modal")
 
-// Your JavaScript code goes here!
+
+//document.addEventListener("DOMContentLoaded", () => {
+
+errorModal.className = "hidden"
+console.log(errorModal.className)
+
+listenClicks()
+
+//})
+ 
+
+function listenClicks(){
+    document.addEventListener('click', (event) => {
+    
+    if(event.target.classList[0] === "like-glyph"){
+
+      mimicServerCall()
+      .then(resp => {
+        const activated = event.target.classList.contains("activated-heart");
+        if(activated){ 
+        event.target.classList.remove("activated-heart")
+        event.target.innerHTML = EMPTY_HEART
+        }else{
+
+          event.target.classList.add("activated-heart")
+          event.target.innerHTML = FULL_HEART
+        }
+        
+        activated;
+      })
+
+      .catch((function(error) {
+        errorModal.className = "";
+        errorModal.innerText = error;
+        setTimeout(() =>  errorModal.className = "hidden", 3000);
+        })
+  
+      )}
+
+
+    });
+
+  }
+
+
+
+
 
 
 
